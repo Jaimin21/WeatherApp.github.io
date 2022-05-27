ede21ae6810bf72c4e7c1fab1c5d97785ed5a027
Jaimin21 <jemiisidd66@gmail.com>
2022-05-27 14:02:33 +0530

    Weather app

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,8 @@
+export const environment = {
+  production: true,
+  weatherApiBaseUrl: 'https://community-open-weather-map.p.rapidapi.com/weather',
+  XRapidAPIHostHeaderName: 'X-RapidAPI-Host',
+  XRapidAPIHostHeaderValue: 'community-open-weather-map.p.rapidapi.com',
+  XRapidAPIKeyHeaderName:   'X-RapidAPI-Key',
+  XRapidAPIKeyHeaderValue: 'a6d3d2682dmsh82cb56a8d383d38p1306c3jsnf0bfdf07006b'
+};
